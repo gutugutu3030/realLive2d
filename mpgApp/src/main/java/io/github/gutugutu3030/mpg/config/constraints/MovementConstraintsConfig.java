d2183bc5c83a429a167ffbbe0b00141edac42e96
gutugutu3030 <2537302+gutugutu3030@users.noreply.github.com>
2021-05-09 17:34:06 +0900

    Layerクラスの記述追加

--- mpgApp/src/main/java/io/github/gutugutu3030/mpg/config/constraints/MovementConstraintsConfig.java
+++ mpgApp/src/main/java/io/github/gutugutu3030/mpg/config/constraints/MovementConstraintsConfig.java
@@ -1,6 +1,7 @@
 package io.github.gutugutu3030.mpg.config.constraints;
 
 import io.github.gutugutu3030.config.AbstractConfig;
+import io.github.gutugutu3030.util.Vector;
 
 /**
  * 傾きと平行移動の制約条件 [平行移動のMAX（mm）] = [傾き（度）] x a + b
@@ -8,4 +9,15 @@ import io.github.gutugutu3030.config.AbstractConfig;
 public class MovementConstraintsConfig extends AbstractConfig {
     public double a = -1.6;
     public double b = 40;
+
+    /**
+     * 制約条件に移動量を傾きがあっているか調べます
+     * 
+     * @param position 移動量
+     * @param angle    回転量
+     * @return 制約をみたしているか
+     */
+    public boolean met(Vector position, double angle) {
+        return position.mag() <= angle * a + b;
+    }
 }
---
+++ mpgApp/src/main/java/io/github/gutugutu3030/mpg/layer/servo/ServoPWM.java
@@ -0,0 +1,15 @@
+package io.github.gutugutu3030.mpg.layer.servo;
+
+/**
+ * サーボモータのPWMの範囲
+ */
+public class ServoPWM {
+    /**
+     * 最小値(0度)
+     */
+    public int pwmMin;
+    /**
+     * 最大値(90度)
+     */
+    public int pwmMax;
+}
